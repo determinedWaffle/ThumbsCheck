f093cc7ff7d86bf8504bd90e1519f0d6456398d9
Jun(Mike) Yao <yaojunmike@juns-MacBook-Pro-2.local>
2015-04-30 16:10:05 -0700

    setting up

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+angular.module('thumbsCheckApp', [
+  'ngRoute'
+
+]);
---
+++ app/scripts/routes.js
@@ -0,0 +1,9 @@
+angular.module('thumbsCheckApp')
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({redirectTo: '/'});
+  }])
