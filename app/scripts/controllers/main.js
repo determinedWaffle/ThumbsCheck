63cd1de68324296fe14bc963a5878aa07eca9624
David Trinh <dhtrinh02@gmail.com>
2015-04-30 20:20:18 -0700

    set guidelines for firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,9 @@
 angular.module('thumbsCheckApp')
-  .controller('MainCtrl', function($scope, exampleService) {
+  // inject $firebaseObject
+  .controller('MainCtrl', function($scope, exampleService, $firebaseObject) {
+    // https://www.firebase.com/docs/web/libraries/angular/quickstart.html 
+    // create a new firebase connection as below;
+    var ref = new Firebase('https://waffleup.firebaseio.com');
     $scope.input = '';
     $scope.redirect = function() {
       exampleService.redirect();
