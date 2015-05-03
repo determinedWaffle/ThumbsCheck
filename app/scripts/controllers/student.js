d095039ebb8c6bcc4183e497ae22f69c41cf362c
David Trinh <dhtrinh02@gmail.com>
2015-05-02 17:25:26 -0700

    got userId to be stored on firebase   keep key with github attached   make value as username

--- app/scripts/controllers/student.js
+++ app/scripts/controllers/student.js
@@ -1,14 +1,28 @@
 angular.module('thumbsCheckApp')
-  .controller('StudentCtrl', function($scope, $firebaseObject, $firebase){
-    var ref = new Firebase('https://waffleup.firebaseio.com/');    
-    var responsesRef = ref.child('responses'); // collection within the database.
 
-    $scope.student1ID= '1';
-    $scope.student2ID = '2';
+  .controller('StudentCtrl', function($scope, user, Auth, Ref, $firebaseObject, $firebase) {
+    var studentObj = $firebaseObject(new Firebase('https://waffleup.firebaseio.com/users'));
+    $scope.user = user;
+    $scope.logout = function() {
+      Auth.$unauth();
+    };
+    $scope.messages = [];
+    // var profile = $firebaseObject(Ref.child('users/' + user.uid));
+    // profile.$bindTo($scope, 'profile');
 
-    $firebaseObject(responsesRef.child($scope.student1ID)).$bindTo($scope, 'student1');
+    
+    studentObj[user.uid] = user.uid;
+    studentObj.$save();
 
-    $firebaseObject(responsesRef.child($scope.student2ID)).$bindTo($scope, 'student2');
+
+    // var profile = $firebaseObject(Ref.child('users/'+user.uid));
+    // profile.$bindTo($scope, 'profile');
+    // $scope.student1ID= '1';
+    // $scope.student2ID = '2';
+
+    // $firebaseobject(responsesref.child($scope.student1id)).$bindto($scope, 'student1');
+
+    // $firebaseObject(responsesRef.child($scope.student2ID)).$bindTo($scope, 'student2');
 
 
     // $scope.students = $firebaseObject(responsesRef);
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -5,7 +5,7 @@ angular.module('thumbsCheckApp')
       route.resolve.user = ['Auth', function(Auth) {
         return Auth.$requireAuth();
       }];
-      $routeProvider.when(path,route);
+      $routeProvider.when(path, route);
       SECURED_ROUTES[path] = true;
       return $routeProvider;
     };
@@ -25,11 +25,11 @@ angular.module('thumbsCheckApp')
         templateUrl: 'views/demo.html',
         controller: 'DemoCtrl'
       })
-      .when('/student', {
+      .whenAuthenticated('/student', {
         templateUrl: 'views/student.html',
         controller: 'StudentCtrl'
       })
-      .when('/instructor', {
+      .whenAuthenticated('/instructor', {
         templateUrl: 'views/instructor.html',
         controller: 'InstructorCtrl'
       })
@@ -38,29 +38,29 @@ angular.module('thumbsCheckApp')
       });
   }])
   .run(['$rootScope', '$location', 'Auth', 'SECURED_ROUTES', 'loginRedirectPath',
-     function($rootScope, $location, Auth, SECURED_ROUTES, loginRedirectPath) {
-       // watch for login status changes and redirect if appropriate
-       Auth.$onAuth(check);
+    function($rootScope, $location, Auth, SECURED_ROUTES, loginRedirectPath) {
+      // watch for login status changes and redirect if appropriate
+      Auth.$onAuth(check);
 
-       // some of our routes may reject resolve promises with the special {authRequired: true} error
-       // this redirects to the login page whenever that is encountered
-       $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
-         if( err === 'AUTH_REQUIRED' ) {
-           $location.path(loginRedirectPath);
-         }
-       });
+      // some of our routes may reject resolve promises with the special {authRequired: true} error
+      // this redirects to the login page whenever that is encountered
+      $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
+        if (err === 'AUTH_REQUIRED') {
+          $location.path(loginRedirectPath);
+        }
+      });
 
-       function check(user) {
-         if( !user && authRequired($location.path()) ) {
-           $location.path(loginRedirectPath);
-         }
-       }
+      function check(user) {
+        if (!user && authRequired($location.path())) {
+          $location.path(loginRedirectPath);
+        }
+      }
 
-       function authRequired(path) {
-         return SECURED_ROUTES.hasOwnProperty(path);
-       }
-     }
-   ])
+      function authRequired(path) {
+        return SECURED_ROUTES.hasOwnProperty(path);
+      }
+    }
+  ])
 
-   // used by route security
-   .constant('SECURED_ROUTES', {});
+  // used by route security
+  .constant('SECURED_ROUTES', {});
