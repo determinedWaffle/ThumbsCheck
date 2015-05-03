2d82f419d842edd0cccb7ba69b2cd90f7473dbfa
Jun(Mike) Yao <yaojunmike@juns-MacBook-Pro-2.local>
2015-05-02 17:24:58 -0700

    working on student view

--- app/scripts/controllers/student.js
+++ app/scripts/controllers/student.js
@@ -3,30 +3,16 @@ angular.module('thumbsCheckApp')
     var ref = new Firebase('https://waffleup.firebaseio.com/');
     var responsesRef = ref.child('responses'); // collection within the database.
 
-
-    $scope.student1ID = '1';
-    $scope.student2ID = '2';
-
     $firebaseObject(responsesRef).$bindTo($scope, 'students');
     $scope.students = {};
-
-
+    $scope.sid = 'github123';
+    $scope.student = { 'sid': $scope.sid , 'response': $scope.response};
     // $firebaseObject(responsesRef).$bindTo($scope, 'student2');
 
 
     $scope.logout = function() {
       Auth.$unauth();
     };
-    // $scope.students = $firebaseObject(responsesRef);
-    $scope.generate = function(){
-     var count = 100;
-     while(count--){
-      $scope.students['sid' + count] = Math.random();
-        }
-    };
-
-    console.log('$scope.students:', $scope.students);
-
-    $firebaseObject(responsesRef).$bindTo($scope, 'students');
+    $firebaseObject(responsesRef).$bindTo($scope, 'student');
 
   });
