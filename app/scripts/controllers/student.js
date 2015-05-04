d8fb764a836bd3df393cf78e9cc872ae275dcc16
David Trinh <dhtrinh02@gmail.com>
2015-05-03 21:17:43 -0700

    Add logout button into student.html, and log out logic into studentCtrl.

--- app/scripts/controllers/student.js
+++ app/scripts/controllers/student.js
@@ -1,30 +1,19 @@
 angular.module('thumbsCheckApp')
+  .controller('StudentCtrl', function($scope, $firebaseObject, $firebase, Auth) {
+    var ref = new Firebase('https://waffleup.firebaseio.com/');
+    var responsesRef = ref.child('responses'); // collection within the database.
 
-  .controller('StudentCtrl', function($scope, user, Auth, Ref, $firebaseObject, $firebase) {
-    var studentObj = $firebaseObject(new Firebase('https://waffleup.firebaseio.com/users'));
-    $scope.user = user;
+    $scope.student1ID = '1';
+    $scope.student2ID = '2';
+
+    $firebaseObject(responsesRef.child($scope.student1ID)).$bindTo($scope, 'student1');
+
+    $firebaseObject(responsesRef.child($scope.student2ID)).$bindTo($scope, 'student2');
+
+
     $scope.logout = function() {
       Auth.$unauth();
     };
-    $scope.messages = [];
-    // var profile = $firebaseObject(Ref.child('users/' + user.uid));
-    // profile.$bindTo($scope, 'profile');
-
-    
-    studentObj[user.uid] = user.uid;
-    studentObj.$save();
-
-
-    // var profile = $firebaseObject(Ref.child('users/'+user.uid));
-    // profile.$bindTo($scope, 'profile');
-    // $scope.student1ID= '1';
-    // $scope.student2ID = '2';
-
-    // $firebaseobject(responsesref.child($scope.student1id)).$bindto($scope, 'student1');
-
-    // $firebaseObject(responsesRef.child($scope.student2ID)).$bindTo($scope, 'student2');
-
-
     // $scope.students = $firebaseObject(responsesRef);
 
     // var count = 100;
