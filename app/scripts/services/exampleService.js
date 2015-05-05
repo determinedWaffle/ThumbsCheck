eb87eed0276d2f76040ee345ac47654d303eb2ee
Andy Sponring <asponring@gmail.com>
2015-05-05 14:41:08 -0700

    Deleted demo files and exampleService

--- app/scripts/services/exampleService.js
+++
@@ -1,7 +0,0 @@
-﻿// // this is an example to create a service.
-// angular.module('thumbsCheckApp')
-//   .service('exampleService', function($location) {
-//     this.redirect = function() {
-//       $location.path('/');
-//     };
-//   });
