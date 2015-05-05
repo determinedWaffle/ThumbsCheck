07a9397e5abdf2da460759eb0f27e665de28027e
Jun(Mike) Yao <yaojunmike@juns-MacBook-Pro-2.local>
2015-05-04 17:05:49 -0700

    Instructor startNew bring all stduents to thumbsCheck choice page

--- app/scripts/controllers/student.js
+++ app/scripts/controllers/student.js
@@ -3,23 +3,14 @@ angular.module('thumbsCheckApp')
     var ref = new Firebase('https://waffleup.firebaseio.com/');
     // var responsesRef = ref.child('responses'); // collection within the database.
     var triggerRef = ref.child('trigger');
-    $scope.trigger = $firebaseObject(triggerRef);
+
+    var unwatch = $firebaseObject(triggerRef).$watch(function() {
+      $scope.studentTrigger = false;
+      $scope.thumbsChoice = 'middle';
+    });
 
     $scope.clicked = function(){
-      console.log('clicked:' , $scope.radioModel);
-      $scope.trigger.val = !$scope.trigger.val
+      $scope.studentTrigger = !$scope.studentTrigger;
     };
-
-    // $firebaseObject(responsesRef).$bindTo($scope, 'students');
-    // $scope.students = {};
-    // $scope.sid = 'github123';
-    // $scope.student = { 'sid': $scope.sid , 'response': $scope.response};
-    // // $firebaseObject(responsesRef).$bindTo($scope, 'student2');
-
-
-    // $scope.logout = function() {
-    //   Auth.$unauth();
-    // };
-    // $firebaseObject(responsesRef).$bindTo($scope, 'student');
 
   });
