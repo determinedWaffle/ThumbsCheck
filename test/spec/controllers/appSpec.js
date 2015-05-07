f8caee4df444e2a8ea0ce990907ffeabce397543
Andy Sponring <asponring@gmail.com>
2015-05-06 17:16:13 -0700

    Added all stubs needed for testing using karma

---
+++ test/spec/controllers/appSpec.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+describe('Thumbs Check App', function () {
+
+  // load the controller's module
+  beforeEach(module('thumbsCheckApp'));
+
+  var MainCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  // beforeEach(inject(function ($controller, $rootScope) {
+  //   scope = $rootScope.$new();
+  //   MainCtrl = $controller('MainCtrl', {
+  //     $scope: scope
+  //   });
+  // }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    console.log("running tests");
+    expect(true).toBe(true);    
+    // expect(scope.awesomeThings.length).toBe(3);
+  });
+});
