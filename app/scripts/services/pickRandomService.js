fc15b867e99df05d8d1bbc694a3bc263acbd17a7
David Trinh <dhtrinh02@gmail.com>
2015-05-07 21:49:39 -0700

    refactor pickRandom function in instructorCtrl and quizCtrl into a service.

---
+++ app/scripts/services/pickRandomService.js
@@ -0,0 +1,29 @@
+﻿angular.module('thumbsCheckApp')
+  .service('pickRandomService', function($firebaseObject, Ref) {
+    var pickRandom = function(array) {
+      // If this category empty, don't proceed pick a student
+      if (array.length === 0) {
+        return;
+      }
+      
+      // Generate a url path to github avatar 
+      var path = 'https://avatars0.githubusercontent.com/u/';
+      var index = Math.floor(Math.random() * array.length);
+      path += array[index].split(':')[1];
+      path += '?size=1028';
+
+      var uid = array[index];
+      var randomPick = {};
+
+      randomPick.uid = uid;
+      randomPick.path = path;
+      return randomPick;
+    };
+
+    return {
+      pickRandomStudent: function(studentList) {
+        return pickRandom(studentList);
+      }
+    };
+
+  });
