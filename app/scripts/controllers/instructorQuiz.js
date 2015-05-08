c651c7492fd4b303127a2bdfac260e7b35125bbd
David Trinh <dhtrinh02@gmail.com>
2015-05-08 02:08:53 -0700

    fixed type error $valid is null, fixed progressbar when it shows NaN, removed unnecessary file .ngShowAuth.js.swp

--- app/scripts/controllers/instructorQuiz.js
+++ app/scripts/controllers/instructorQuiz.js
@@ -30,7 +30,6 @@ angular.module('thumbsCheckApp')
 
       quiz.question = question;
       quiz.choices = choices;
-      console.log(quiz);
       quizes.$add(quiz);
 
       // clear form
@@ -74,10 +73,7 @@ angular.module('thumbsCheckApp')
     // watch firebase responses, upon change, update counts and studentList
     quizResponsesObj.$loaded().then(function() {
       quizResponsesObj.$watch(function() {
-        // console.log('watch');  var delayInMs = 2000;
           results = $scope.total();
-        // $scope.quizResult = results[0];
-        // $scope.studentList = results[1];
       });
     });
 
@@ -92,20 +88,10 @@ angular.module('thumbsCheckApp')
         quizCounts.push(0);
         studentList[i] = [];
       }
-      // console.log('result initial',quizCounts);
-      // console.log('studentList', studentList);
-
-      // for input of $scope.pickRandom()
-      // console.log('studentList',studentList);
       quizResponsesObj.$loaded().then(function(responses) {
-        // console.log('responses:', responses);
         tallyUpStudentResponsesService.tallyUpResponses(responses, undefined, studentList, quizCounts);
-        // debugger;
-        console.log(quizCounts);
         $scope.populateProgressBar(quizCounts);
         $scope.studentList = studentList;
-        console.log('quizCounts before return', quizCounts);
-        // console.log('studentList', studentList);
       });
     };
 
@@ -127,17 +113,17 @@ angular.module('thumbsCheckApp')
     };
 
     $scope.populateProgressBar = function(quizResult) {
-      // if($scope.quizCounts){return;}
       $scope.stacked = [];
+
       var types = ['success', 'info', 'warning', 'danger'];
-      // $scope.quizResult = [2,50,50];
-      // quizResult
-      // console.log('quizResult progress', quizResult);
+
       var quizCountsTotal = quizResult.reduce(function(memo, x) {
         return memo + x;
       });
 
-      // console.log('counts total',quizCountsTotal);
+      if (quizCountsTotal === 0) {
+        return $scope.stacked;
+      }
 
       quizResult.forEach(function(val, i) {
         var percent = Math.floor((val / quizCountsTotal) * 100);
