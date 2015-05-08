d1240904dcc67865ed577756a49b64d4517c30a9
David Trinh <dhtrinh02@gmail.com>
2015-05-08 00:54:16 -0700

    service for $scope.total function

--- app/scripts/services/tallyUpStudentResponsesService.js
+++ app/scripts/services/tallyUpStudentResponsesService.js
@@ -1,6 +1,7 @@
 angular.module('thumbsCheckApp')
   .service('tallyUpStudentResponsesService', function() {
-    var tallyUpResponses = function(responses, result, studentList) {
+
+    var tallyUpResponses = function(responses, result, studentList, quizCounts) {
       // Make key: $id and $priority non-enumerable
       Object.defineProperty(responses, '$id', {
         enumerable: false
@@ -13,36 +14,42 @@ angular.module('thumbsCheckApp')
       Object.defineProperty(responses, '$$conf', {
         enumerable: false
       });
-      
+
       for (var key in responses) {
         if (responses.hasOwnProperty(key)) {
-          // console.log('key',key);
-          var response = responses[key];
-          // After reset(), on responses obj, there is a key value pair ($value:null)
-          if (response === null) {
-            // Return upon empty responses
-            return [result, studentList];
+          if (quizCounts !== undefined && result === undefined) {
+              response = responses[key][key];
+              console.log('response', response);
+              quizCounts[response] += 1;
+              studentList[response].push(key);
           } else {
-            response = response[key];
-            if (response === 'up') {
-              result[0] += 1;
-              studentList.up.push(key);
-            } else if (response === 'middle') {
-              result[1] += 1;
-              studentList.middle.push(key);
-            } else if (response === 'down') {
-              result[2] += 1;
-              studentList.down.push(key);
+            var response = valueResponses[key];
+            // After reset(), on responses obj, there is a key value pair ($value:null)
+            if (response === null) {
+              // Return upon empty responses
+              return [result, studentList];
+            } else {
+              response = response[key];
+              if (response === 'up') {
+                result[0] += 1;
+                studentList.up.push(key);
+              } else if (response === 'middle') {
+                result[1] += 1;
+                studentList.middle.push(key);
+              } else if (response === 'down') {
+                result[2] += 1;
+                studentList.down.push(key);
+              }
             }
           }
         }
       }
-
     };
 
+
     return {
-      tallyUpResponses: function(responses, result, studentList) {
-        tallyUpResponses(responses, result, studentList);
+      tallyUpResponses: function(responses, result, studentList, quizCounts) {
+        tallyUpResponses(responses, result, studentList, quizCounts);
       }
     };
 
