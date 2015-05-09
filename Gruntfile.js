2e92db9fac9e555b0fd28970380ff6629d4a9ff1
Andy Sponring <asponring@gmail.com>
2015-05-08 22:34:35 -0700

    Configured grunt default to run jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+    jshint: {
+      files: [
+        // Add filespec list here
+        ['Gruntfile.js', 'app/*.js', 'app/**/*.js']
+      ],
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        ignores: [
+          'app/bower_components/**/*.js'
+        ]
       }
+      // options: {
+      //   jshintrc: '.jshintrc'
+      // }
     }
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
   });
 
   // Load the plugin that provides the "uglify" task.
   // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jshint']);
 
 };
