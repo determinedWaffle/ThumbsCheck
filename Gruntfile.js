d43b681ed46a13f6f08e98841b61531b2c4de2fb
Andy Sponring <asponring@gmail.com>
2015-05-09 08:36:10 -0700

    Added deploy tasks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,38 @@ module.exports = function(grunt) {
     jshint: {
       files: [
         // Add filespec list here
-        ['Gruntfile.js', 'app/*.js', 'app/**/*.js']
+        ['Gruntfile.js', 'app/**/*.js', 'server.js']
       ],
       options: {
+        jshintrc: '.jshintrc',
         ignores: [
           'app/bower_components/**/*.js'
         ]
       }
-      // options: {
-      //   jshintrc: '.jshintrc'
-      // }
+    },
+    
+    watch: {
+      scripts: {
+        files: ['app/**/*.js', 'server.js'],
+        tasks: ['jshint']
+      }
+    },
+
+    shell: {
+      deployProduction: {
+        command: 'firebase deploy',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
     }
     // uglify: {
     //   options: {
@@ -28,11 +50,33 @@ module.exports = function(grunt) {
     // }
   });
 
-  // Load the plugin that provides the "uglify" task.
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
 
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  grunt.registerTask('deploy', function(n) {
+    if(grunt.option('prod')) {
+      grunt.task.run([ 'jshint', 'shell:deployProduction' ]);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
 };
